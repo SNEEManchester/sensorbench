669c019c36f5999d74c0cc5533eec938bdad7c5e
alan.barry.stokes <alan.barry.stokes@dec5688c-dbed-6d27-e9c9-47ba4483a259>
2013-05-03 15:24:35 +0000

    Script to generate the condor submit file needed by condor to generate jobs

--- trunk/condorScriptGenerator.py
+++ trunk/condorScriptGenerator.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python
-import os, sys, networkLib, math, getopt, UtilLib
+import os, sys, networkLib, math, getopt, UtilLib, CSVLib, shutil
 
-noInstances = 10
+optNumInstances = 10
+optCondorDir = "condor"
+optBinaryDir = "avrora-Bineries"
+pathSeperator = os.sep
 
+optExprList = ["1a", "1b", "2a", "2b", "3a", "3b", "4a", "4b", "5a", "5b", "6a", "6b", "7"]
+optPlatList = ["INSNEE", "HC", "TinyDB", "MHOCS"]
 def parseArgs(args):	
 	global optScenarioDir
 	try:
-		optNames = ["scenario-dir="]
+		optNames = ["condor-dir=", "num-instances=", "binary-dir="]
 	
 		#append the result of getOpNames to all the libraries 
 		optNames = UtilLib.removeDuplicates(optNames)
@@ -18,46 +23,45 @@ def parseArgs(args):
 		sys.exit(2)
 			
 	for o, a in opts:
-		if (o == "--scenario-dir"):
-			optScenarioDir = a
+		if (o == "--condor-dir"):
+			optCondorDir = a
+		elif (o == "--num-instances"):
+			optNumInstances = int(a)
+		elif (o == "--binary-dir"):
+			optBinaryDir = a
 		else:
 			usage()
 			sys.exit(2)
 
 
 def usage():
-		print "generate-scenarios.py --scenario-dir=<dir>"
+	print "generate-scenarios.py --condor-dir=<dir> --num-instances=<int> --binary-dir=<dir>"
+
 
 def check_dir(d):
 	if not os.path.exists(d):
 		os.makedirs(d)
 
 def generateTopBlurb():
-	open("test.txt", "w") as condorFile
-  condorFile.write("universe = vanilla \n executable = start.sh \n when_to_transfer_output = ON_EXIT \n Should_Transfer_Files = YES \n transfer_input_files = ../SNEE.jar,../jre.tar.gz
-Requirements = (OpSys == "LINUX") &&(HAS_STANDARD_IMAGE =?= True)
-Request_Disk = 3000000 
-request_memory = 2048 
-#Output = output$(Process).txt 
-#Error = error$(Process).txt 
-log = log.txt 
-Output = out.txt 
-Error = err.txt 
-notification = error 
+	check_dir(optCondorDir)
+	condorFile = open(optCondorDir + pathSeperator + "submit.txt", "w") 
+	condorFile.write("universe = vanilla \n executable = start.sh \n when_to_transfer_output = ON_EXIT \n Should_Transfer_Files = YES \n transfer_input_files = ../SNEE.jar,../jre.tar.gz \n Requirements = (OpSys == \"LINUX\") &&(HAS_STANDARD_IMAGE =?= True) \n Request_Disk = 3000000 \n request_memory = 2048 \n #Output = output$(Process).txt \n #Error = error$(Process).txt \n log = log.txt \n Output = out.txt \n Error = err.txt \n notification = error \n\n\n")
 	
 
 
 
-def condorLine(task,xVals,xValLabels,xValAttr,exprAttr,runAttrCols,outputDir):
-	
+def condorLine(task,xVals,xValLabels,xValAttr,exprAttr):
+	condorFile = open(optCondorDir + pathSeperator + "submit.txt", "a") 
+	CONVENTION ="CONVENTION"
+	binaryFolderName = optBinaryDir + pathSeperator + CONVENTION
+	condorFile.write("Arguments = %s \n	initialdir   = %s \n	queue \n\n"%(binaryFolderName, CONVENTION))
+	check_dir(optCondorDir + pathSeperator + CONVENTION)
 
 
 
 
-def generateScriptForEachJob(exprAttr, exprAttrCols, outputDir):
+def generateScriptForEachJob(exprAttr, exprAttrCols):
 	global optPlatList, noInstances
-
-	runAttrCols = exprAttrCols + ["BufferingFactor", "Platform", "Task", "xvalLabel", "SNEEExitCode", "NetworkSize", "Layout", "NetworkDensity","NetworkPercentSources", "SimulationDuration", "Tuple Acq Count", "Tuple Del Count", "Tuple Delta Sum", "Data Freshness", "Output Rate", "Delivery Rate", "Sum Energy", "Sum Energy 6M", "Max Energy", "Average Energy", "CPU Energy", "Sensor Energy", "Other Energy", "Network Lifetime secs", "Network Lifetime days", "Comments"]
 
 	tasks = exprAttr["Tasks"].split(";")
 	xValAttr = exprAttr["XvalAttr"]
@@ -66,8 +70,8 @@ def generateScriptForEachJob(exprAttr, exprAttrCols, outputDir):
 
 	for plat in optPlatList:	
 		for task in tasks:
-			for instance in range(1,noInstances):
-				condorLine(task,xVals,xValLabels,xValAttr,exprAttr,runAttrCols,outputDir)
+			for instance in range(1,optNumInstances):
+				condorLine(task,xVals,xValLabels,xValAttr,exprAttr)
 
 def generateScript():
 	colNames = None
@@ -85,11 +89,15 @@ def generateScript():
 		if not str(exprAttr['Experiment']) in optExprList:
 			continue
 
-		exprAttr['TimeStamp']=timeStamp
 		#Experiment,X,Y,Tasks,Xlabels,Network,RadioLossRate,AcquisitionRate
 		generateTopBlurb()
-		generateScriptForEachJob(exprAttr, exprAttrCols, outputDir)
+		generateScriptForEachJob(exprAttr, exprAttrCols)
 
+
+def moveCollections():
+	shutil.copyfile(optBinaryDir+".zip", optCondorDir + pathSeperator + optBinaryDir+".zip")
+	shutil.copyfile("avrora-1.7.113.jar", optCondorDir + pathSeperator + "avrora-1.7.113.jar")
+	shutil.copyfile("jre.tar.gz", optCondorDir + pathSeperator + "jre.tar.gz")
 
 
 def main(): 	
@@ -98,6 +106,7 @@ def main():
   #parse the command-line arguments
 	parseArgs(sys.argv[1:]) 
 	generateScript()
+	moveCollections()
   
 
 

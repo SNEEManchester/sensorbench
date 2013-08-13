1dee70ea234b47bb4205712e43c31313a5b475ba
ixentg@gmail.com <ixentg@gmail.com@dec5688c-dbed-6d27-e9c9-47ba4483a259>
2013-08-13 16:12:04 +0000

    CSV result generation

--- trunk/lib/SBLib.py
+++ trunk/lib/SBLib.py
@@ -2,6 +2,7 @@
 
 import os, CSVLib
 
+#Logs the contents of RunAttr hash for single file
 def logResultsToFile(runAttr, runAttrCols, resultsFileName):
 	if not os.path.exists(resultsFileName):
 		resultsFile = open(resultsFileName, "w")
@@ -12,6 +13,7 @@ def logResultsToFile(runAttr, runAttrCols, resultsFileName):
 	resultsFile.close()
 
 
+#Logs the contents of RunAttr hash (one global file, one per experiment/platform)
 def logResultsToFiles(runAttr, runAttrCols, outputDir):
 	#Per-experiment/plaftform results file
 	resultsFileName = outputDir+os.sep+"exp"+runAttr["Experiment"]+"-"+runAttr["Platform"]+"-results.csv"
@@ -21,7 +23,25 @@ def logResultsToFiles(runAttr, runAttrCols, outputDir):
 	logResultsToFile(runAttr, runAttrCols, resultsFileName)
 
 
+#Returns dirname according to convention
 def getRunOutputDir(runAttr):
 	return "exp"+runAttr["Experiment"]+"-"+runAttr["Platform"]+"-x"+runAttr["xvalLabel"]+"-"+runAttr["Task"]+"-"+str(runAttr["Instance"])
 
+
+#copies parsed values from Avrora simulation into runAttr hash table
+def getAvroraEnergyValues(avroraLogFile, runAttr):
+
+	simulationDuration = runAttr["SimulationDuration"]
+	(sumEnergy, maxEnergy, averageEnergy, radioEnergy, cpu_cycleEnergy, sensorEnergy, otherEnergy, networkLifetime) = AvroraLib.computeEnergyValues(".", simulationDuration, avroraLogFile, ignoreLedEnergy = True, defaultSiteEnergyStock = 31320, siteLifetimeRankFile = None, sink = 0, ignoreList = [])
+	#All node energy in Joules for simulation duration
+	runAttr["Sum Energy"] = sumEnergy
+        #All node energy in Joules scaled to 6 month period
+	runAttr["Sum Energy 6M"] = sumEnergy*((60.0*60.0*24.0*30.0*6.0)/float(simulationDuration))
+	runAttr["Max Energy"] = maxEnergy
+	runAttr["Average Energy"] = averageEnergy
+	runAttr["CPU Energy"] = cpu_cycleEnergy
+	runAttr["Sensor Energy"] = sensorEnergy
+	runAttr["Other Energy"] = otherEnergy
+	runAttr["Network Lifetime secs"] = networkLifetime
+	runAttr["Network Lifetime days"] = float(networkLifetime)/60.0/60.0/24.0
 

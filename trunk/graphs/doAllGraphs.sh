8b659f5f56074b5cd0b562e6b0d5113371bf2577
ixentg@gmail.com <ixentg@gmail.com@dec5688c-dbed-6d27-e9c9-47ba4483a259>
2013-08-16 15:53:49 +0000

    all graphs script

--- trunk/graphs/doAllGraphs.sh
+++ trunk/graphs/doAllGraphs.sh
@@ -1,50 +1,9 @@
 
-
-gnuplot < exp0-delivery-rate.gnuplot
-gnuplot	< exp0-output-rate.gnuplot
-gnuplot < exp0-freshness.gnuplot
-gnuplot < exp0-sum-6m-energy.gnuplot
-gnuplot < exp0-lifetime.gnuplot
-
-gnuplot < exp1a-delivery-rate.gnuplot
-gnuplot	< exp1a-output-rate.gnuplot
-gnuplot < exp1a-freshness.gnuplot
-gnuplot < exp1a-sum-6m-energy.gnuplot
-gnuplot < exp1a-lifetime.gnuplot
-
-gnuplot < exp2a-delivery-rate.gnuplot
-gnuplot	< exp2a-output-rate.gnuplot
-gnuplot < exp2a-freshness.gnuplot
-gnuplot < exp2a-sum-6m-energy.gnuplot
-gnuplot < exp2a-lifetime.gnuplot
-
-gnuplot < exp3a-delivery-rate.gnuplot
-gnuplot	< exp3a-output-rate.gnuplot
-gnuplot < exp3a-freshness.gnuplot
-gnuplot < exp3a-sum-6m-energy.gnuplot
-gnuplot < exp3a-lifetime.gnuplot
-
-gnuplot < exp4a-delivery-rate.gnuplot
-gnuplot	< exp4a-output-rate.gnuplot
-gnuplot < exp4a-freshness.gnuplot
-gnuplot < exp4a-sum-6m-energy.gnuplot
-gnuplot < exp4a-lifetime.gnuplot
-
-gnuplot < exp5a-delivery-rate.gnuplot
-gnuplot	< exp5a-output-rate.gnuplot
-gnuplot < exp5a-freshness.gnuplot
-gnuplot < exp5a-sum-6m-energy.gnuplot
-gnuplot < exp5a-lifetime.gnuplot
-
-gnuplot < exp6a-delivery-rate.gnuplot
-gnuplot	< exp6a-output-rate.gnuplot
-gnuplot < exp6a-freshness.gnuplot
-gnuplot < exp6a-sum-6m-energy.gnuplot
-gnuplot < exp6a-lifetime.gnuplot
-
-gnuplot < exp7-delivery-rate.gnuplot
-gnuplot	< exp7-output-rate.gnuplot
-gnuplot < exp7-freshness.gnuplot
-gnuplot < exp7-sum-6m-energy.gnuplot
-gnuplot < exp7-lifetime.gnuplot
+./doExpGraphs.sh 1a
+./doExpGraphs.sh 2a
+./doExpGraphs.sh 3a
+./doExpGraphs.sh 4a
+./doExpGraphs.sh 5a
+./doExpGraphs.sh 6a
+./doExpGraphs.sh 7
 

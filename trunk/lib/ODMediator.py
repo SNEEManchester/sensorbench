2fcd6b10d5a1ea8eb06d8fbbb128116b1fade06b
alan.barry.stokes <alan.barry.stokes@dec5688c-dbed-6d27-e9c9-47ba4483a259>
2013-09-24 16:15:00 +0000

    modified ODMediator so that it uses generated binaries and outputs correct avrora strings

--- trunk/lib/ODMediator.py
+++ trunk/lib/ODMediator.py
@@ -2,10 +2,10 @@ import os, shutil, sys, re, string, AvroraLib
 
 OD_BETA = 1 #Outlier Detection Buffering factor
 
-def init(scenarioDir, sourceDir):
-	global optScenarioDir, optSourceRootDir
+def init(scenarioDir, avroraElfRootDir):
+	global optScenarioDir, optAvroraElfDir
 	optScenarioDir = scenarioDir
-	optSourceRootDir = sourceDir
+	optAvroraElfDir= avroraElfRootDir
 	#Compile ELF binaries. Binaries are assumed to be already compiled
 
 
@@ -30,47 +30,45 @@ def getAvroraCommandString(runAttr, runAttrCols, avroraElfDir):
 	nodeCountStr = runAttr['NetworkSize']
 	elfString = getODElfFilename(runAttr)
 
-	commandStr = "avrora.Main -mcu=mts300 -platform=micaz -simulation=sensor-network -seconds=%d %s -monitors=leds,packet,energy,c-print -ports=0:0:2390 -random-seed=1 -sensor-data=%s -report-seconds -colors=false -nodecount=%d  %s" % (simDuration, topologyStr, sensorDataString, nodeCountStr, elfString)
+	commandStr = "avrora.Main -mcu=mts300 -platform=micaz -simulation=sensor-network -seconds=100 -monitors=leds,packet,energy,c-print -colors=false -random-seed=1 -sensor-data="
+	numberOfNodesInDeployment = runAttr['NetworkSize']
+	for nodeid in range(1,numberOfNodesInDeployment):
+		if(nodeid = numberOfNodesInDeployment):
+			commandStr = commandStr + "light:"+ str(nodeid) + ":temper" + str(nodeid) + ".txt"
+		else:
+			commandStr = commandStr + "light:"+ str(nodeid) + ":temper" + str(nodeid) + ".txt,"
+
+	commandStr = commandStr + " -report-seconds -nodecount="
+
+	for nodeid in range(0,numberOfNodesInDeployment):
+		if(nodeid = numberOfNodesInDeployment):
+			commandStr = commandStr + "1 "
+		else
+			commandStr = commandStr + "1,"
+
+	for nodeid in range(0,numberOfNodesInDeployment):
+		commandStr = commandStr + "mote" + str(nodeid) + ".elf"
 	return commandStr
 
 
 def generateAvroraJob(task,xVal,xValLabel,xValAttr,instance,runAttr,runAttrCols,rootOutputDir, runOutputDir, avroraJobsRootDir):
-	global optScenarioDir, optSourceRootDir
+	global optScenarioDir, optAvroraElfDir
 
 	#3 Create AvroraJobs folder
 	#create dir for Avrora Job
 	avroraJobDir = avroraJobsRootDir+os.sep+runOutputDir 
 	os.makedirs(avroraJobDir)
 	
-	#generate header files from snee RT
-	generateNodeHeaderFiles(SNEEMediator.defineTempSneeFilesRootDir(), avroraJobDir)
-	
-  #copy correct setup file into outputFolder as D3gen.h file (this contains all the hard coded variables for OD, such as acquisition rate etc
-	copyCorrectDataFilesFromStore(avroraJobDir)
-
-	#compile bineries for OD
-
-	#Copy top file for avrora
-	avroraTopFile = optScenarioDir + os.sep + runAttr['AvroraTopologyFilename']
-	shutil.copy(avroraTopFile, avroraJobDir)
+	#Copy elf files to Avrora Job dir
+	if (os.path.exists(optAvroraElfDir)):
+		f = getODElfFilename(runAttr)
+		shutil.copy(optAvroraElfDir + os.sep + f, avroraJobDir)
 			    
 	#Create Avrora CommandString.txt
-	avroraCommandStr = getAvroraCommandString(runAttr, runAttrCols, optSourceRootDir)
+	avroraCommandStr = getAvroraCommandString(runAttr, runAttrCols, optAvroraElfDir)
 	avroraCommandStrFileName = avroraJobDir + os.sep + "avroraCommandString.txt"
 	avroraCommandStrFile = open(avroraCommandStrFileName, "w")
 	avroraCommandStrFile.writelines(avroraCommandStr)
-
-def generateNodeHeaderFiles(tempSneeFolder, avroraJobDir):
-	print "should generate node header files now"
-
-
-def copyCorrectDataFilesFromStore(avroraJobDir):
-	print "should by copying correct files from store now"
-	shutil.copy(optSourceRootDir+os.sep+"elf"+os.sep+"D3AppC.nc", avroraJobDir)
-	shutil.copy(optSourceRootDir+os.sep+"elf"+os.sep+"D3C.nc", avroraJobDir)
-	shutil.copy(optSourceRootDir+os.sep+"elf"+os.sep+"Makefile", avroraJobDir)
-	shutil.copy(optSourceRootDir+os.sep+"elf"+os.sep+"AvroraPrint.h", avroraJobDir)
-
 
 #TODO: PROBABLY DOESN'T GO HERE
 def runODInAvrora(runAttr, outputDir):

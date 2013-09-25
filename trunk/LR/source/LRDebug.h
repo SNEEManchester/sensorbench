7c4d162fa4e0bee531e79470a3d194f7f68c12fd
alan.barry.stokes <alan.barry.stokes@dec5688c-dbed-6d27-e9c9-47ba4483a259>
2013-09-25 14:56:13 +0000

    attaching source code for LR elfs

---
+++ trunk/LR/source/LRDebug.h
@@ -0,0 +1,24 @@
+/* This is the header file that 
+*
+*
+* @date:	25 February 2011
+* @revision: 	1.0
+* @author: 	George Valkanas ( http://www.di.uoa.gr/~gvalk , gvalk@di.uoa.gr )
+* 		National and Kapodistrian University of Athens,
+* 		Dept. Informatics & Telecommunications
+*		Knowledge Data Discovery Group ( http://kddlab.di.uoa.gr )
+*/
+
+#ifndef LR_NAIVE_DBG_H
+#define LR_NAIVE_DBG_H
+
+#include "AvroraPrint.h"
+
+// Whether we are in debug or in normal execution mode
+#define DEBUG 0
+
+/* Defines whether we will be printing the (a,b) values at the end of each round */
+#define PRINT_AB 0
+
+#endif
+

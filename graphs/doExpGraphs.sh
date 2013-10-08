128b7d3933fb1eae344c66b15d5ea921f93b561b
ixentg@gmail.com <ixentg@gmail.com@dec5688c-dbed-6d27-e9c9-47ba4483a259>
2013-10-08 16:34:00 +0000

    added doExpGraphs script

---
+++ graphs/doExpGraphs.sh
@@ -0,0 +1,8 @@
+
+gnuplot < gnuplot/exp$1-delivery-rate.gnuplot
+gnuplot	< gnuplot/exp$1-output-rate.gnuplot
+gnuplot < gnuplot/exp$1-freshness.gnuplot
+gnuplot < gnuplot/exp$1-sum-6m-energy.gnuplot
+gnuplot < gnuplot/exp$1-lifetime.gnuplot
+
+
